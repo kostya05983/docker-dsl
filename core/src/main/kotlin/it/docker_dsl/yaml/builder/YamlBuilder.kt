0dcb38258b1138ec86e8e55aacecf9d29ac9254c
kostya05983 <kostya05983@mail.ru>
2019-03-01 11:19:45 +0700

    end with yamlBuilder

--- core/src/main/kotlin/it/docker_dsl/yaml/builder/YamlBuilder.kt
+++ core/src/main/kotlin/it/docker_dsl/yaml/builder/YamlBuilder.kt
@@ -1,5 +1,91 @@
 package it.docker_dsl.yaml.builder
 
+/**
+ * class for yaml buildings
+ * @author kostya05983
+ */
 class YamlBuilder {
+    companion object {
+        const val DEVIDER_WITH_NEWLINE = ":\n"
+        const val DEVIDER = ": "
+        const val INCREMENT_INDENT = "  "
+        const val ARRAY_KEY = "- "
+    }
+
+    var currentIndent = ""
+    var amountIndent = 0
+
     val sb = StringBuilder()
+
+
+    /**
+     * @param - tag which is wrote left
+     * example
+     * val yb = YamlBuilder()
+     * yb.writeTag("version")
+     * println(yb.toString)
+     * Output:
+     * version:
+     */
+    fun writeTag(tag: String) {
+        sb.append(tag).append(DEVIDER_WITH_NEWLINE)
+    }
+
+    /**
+     * @param tag - which is wrote left
+     * @param value - value write right in yaml files from tag
+     * Example:
+     * val yb = YamlBuilder()
+     * yb.writeTag("version", "'3'")
+     * println(yb.toString())
+     * Output:
+     * version: '3'
+     */
+    fun writeTag(tag: String, value: String) {
+        sb.append(tag).append(DEVIDER).append(value).append("\n")
+    }
+
+    /**
+     * @param tag - writes left in yaml, key
+     * @param list - array values of key
+     * Example:
+     * val yb = YamlBuilder()
+     * yb.writeTagArray("ports", "/"5000:5000/"")
+     * Output:
+     * ports:
+     *   - "5000:5000"
+     */
+    fun writeTagArray(tag: String, vararg list: String) {
+        sb.append(tag).append(DEVIDER_WITH_NEWLINE)
+        indent()
+        for (value in list) {
+            sb.append(currentIndent).append(ARRAY_KEY)
+                    .append(value)
+            if (list.last() != value) {
+                sb.append("\n")
+            }
+        }
+        dedent()
+    }
+
+    /**
+     * increment indent in yaml file
+     */
+    fun indent() {
+        amountIndent++
+        currentIndent += INCREMENT_INDENT
+    }
+
+    /**
+     * decrement, makes DEDENT in yaml file
+     */
+    fun dedent() {
+        amountIndent--
+        for (i in 0 until amountIndent)
+            currentIndent += INCREMENT_INDENT
+    }
+
+    override fun toString(): String {
+        return sb.toString()
+    }
 }
--- core/src/test/kotlin/YamlBuilderTest.kt
+++ core/src/test/kotlin/YamlBuilderTest.kt
@@ -1,36 +1,65 @@
 import it.docker_dsl.yaml.builder.YamlBuilder
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.FileReader
 
 class YamlBuilderTest {
 
 
     /**
      * version: '3'
-     *   services:
+     * services:
      *   web:
-     *   build: ""
-     *   ports:
-     *   - "5000:5000"
-    8   redis:
-     *   image: "redis:alpine"
+     *     build: ""
+     *     ports:
+     *       - "5000:5000"
+     *   redis:
+     *     image: "redis:alpine"
      */
     @Test
     fun example1() {
+        val fr = FileReader("src/test/resources/docker_compose/example1.yml")
+        val yamlFile = fr.readLines().joinToString("\n")
 
+        val yb = YamlBuilder()
+        yb.writeTag("version", "'3'")
+        yb.writeTag("services")
+        yb.indent()
+        yb.writeTag("web")
+        yb.indent()
+        yb.writeTag("build", "\"\"")
+        yb.writeTagArray("ports", "\"5000:5000\"")
+        yb.dedent()
+        yb.writeTag("redis")
+        yb.indent()
+        yb.writeTag("image", "\"redis:alpine\"")
     }
 
     @Test
-    fun addingIndent() {
-        val yamlBuilder = YamlBuilder()
-        yamlBuilder.writeTag("version", "'3'")
+    fun addingTag() {
+        val yb = YamlBuilder()
+        yb.writeTag("version")
 
-        yamlBuilder.addIndent()
-        val sb = StringBuilder().append("version: '3'")
-                .append("\n  ")
-        assertEquals(yamlBuilder.toString(), sb.toString())
+        val sb = StringBuilder().append("version:\n")
+        assertEquals(sb.toString(), yb.toString())
     }
 
+    @Test
+    fun addingTagValue() {
+        val yb = YamlBuilder()
+        yb.writeTag("version", "'3'")
 
+        val sb = StringBuilder().append("version: '3'\n")
+        assertEquals(sb.toString(), yb.toString())
+    }
+
+    @Test
+    fun writeTagArray() {
+        val yb = YamlBuilder()
+        yb.writeTagArray("ports", "\"5000:5000\"")
+
+        val sb = StringBuilder().append("ports:\n")
+                .append("  - \"5000:5000\"")
+        assertEquals(sb.toString(), yb.toString())
+    }
 }
